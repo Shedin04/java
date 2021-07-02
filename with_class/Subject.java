c3781872c9c5f1202c59a64ae221fe61831ad0f4
Shedin04 <43680396+Shedin04@users.noreply.github.com>
2021-07-02 17:08:35 +0300

    Create Subject.java

---
+++ with_class/Subject.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Subject {
+    public String sub_name;
+    public int sub_rate;
+    public int sub_cr;
+
+    public Subject(String sub_name, int sub_rate, int sub_cr) {
+        this.sub_name = sub_name;
+        this.sub_rate = sub_rate;
+        this.sub_cr = sub_cr;
+    }
+}
